19468bb303b55805f5327298aea4e6338caa7563
yurizav <86908224+yurizav@users.noreply.github.com>
2022-02-17 22:05:53 -0300

    commit1
    correção

--- Java_teste1.java
+++ Java_teste1.java
@@ -9,17 +9,19 @@ public class Java_teste1 {
     
     public static void main(String[] args) {
         
-         //Declara variáveis 
+    //Declara variáveis 
     String nome,endereço, telefone;
     
-    //Armazena valores nas variáveis
+    //Recebe e Armazena valores nas variáveis
         nome = JOptionPane.showInputDialog("Qual é o seu nome ?");
         endereço = JOptionPane.showInputDialog("Qual é o seu endereço ?");
         telefone = JOptionPane.showInputDialog("Qual é o seu telefone ?");
+        
+    //Cria uma caixa de diálogo para devolver os dados recebidos pelo showInputDialog!
                      
-        System.out.println(nome);
-        System.out.println(endereço);
-        System.out.println(telefone);
+        JOptionPane.showMessageDialog(null,"Seu nome é " + nome + "!");
+        JOptionPane.showMessageDialog(null, "Você mora em "+ endereço + "!");
+        JOptionPane.showMessageDialog(null, "Seu telefone é " + telefone + "!");
        
     }
     
